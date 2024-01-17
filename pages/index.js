7a0ef9cc9a9f857bbcdb9edbae5125d65bfb633f
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-17 18:05:10 +0100

    add Bonus

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
 import BoxWithStyledComponents from "../components/BoxWithStyledComponents";
+import styled from "styled-components";
+
+const HorizontalContainer = styled.div`
+  display: flex;
+`;
 
 export default function HomePage() {
   return (
-    <div>
-      {/* BoxWithClassName components */}
+    <HorizontalContainer>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-
-      {/* BoxWithStyledComponents components */}
       <BoxWithStyledComponents />
       <BoxWithStyledComponents $isBlack />
-    </div>
+    </HorizontalContainer>
   );
 }
